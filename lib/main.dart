31f9f2efee2eea7f85829c8389a30bcda62907d2
opas350 <opas350@gmail.com>
2019-06-17 01:01:48 -0500

    Add changes to http Get - Not functional yet

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,17 @@
 // This sample shows an [AppBar] with two simple actions. The first action
 // opens a [SnackBar], while the second action navigates to a new page.
 
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+void main() {
+//debugPaintSizeEnabled=true;
+runApp(MyApp());
+}
 
 /// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
@@ -15,9 +23,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: _title,
       theme: ThemeData(
-        accentColor: Colors.red,
-        primarySwatch: Colors.red
+          accentColor: Colors.red,
+          primarySwatch: Colors.red
       ),
+//      debugShowCheckedModeBanner: true,
+//      debugShowMaterialGrid: true,
       home: MyStatelessWidget(),
     );
   }
@@ -94,11 +104,77 @@ class HomePageContainer extends StatelessWidget{
               leading: Icon(Icons.settings),
               title: Text('Settings'),
               subtitle: Text('Local host and API key'),
+            ),
+            ButtonTheme.bar(
+              child: ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: const Text('Test'),
+                    onPressed: (){
+                      scaffoldKey.currentState.showSnackBar(snackBar);
+                    },
+                  ),
+                  GetReq(),
+                ],
+              ),
             )
           ],
         ),
       ),
     );
   }
+}
 
+Future<http.Response> fetchGet() async {
+  print("In future");
+  final response = await http.get(
+      'https://jsonplaceholder.typicode.com/post/1');
+
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw Exception('Failed to get the information');
+  }
+}
+
+class Post{
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json){
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body']
+    );
+  }
+}
+
+
+class GetReq extends StatelessWidget {
+  final Future<Post> post;
+
+  const GetReq({Key key, this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Post>(
+      future: post,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if(snapshot.hasData){
+          return Text(snapshot.data.title);
+        } else if(snapshot.hasError){
+          return Text("${snapshot.error}");
+        }
+        print('DATA');
+        print(snapshot.data);
+        return CircularProgressIndicator();
+      },
+    );
+  }
 }
